5c30bf3de0c38fa50d22270d554214118fbb2f1a
Kelven Galvão <kelvengalvao@gmail.com>
2020-01-19 14:03:45 -0300

    Remove unused functions

--- lib/src/home/widgets/movie_list/movie_list_controller.dart
+++ lib/src/home/widgets/movie_list/movie_list_controller.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mobx/mobx.dart';
 import 'package:pipok_app/src/models/movie_list_model.dart';
 
@@ -8,8 +9,6 @@ class MovieListController = _MovieListController with _$MovieListController;
 
 abstract class _MovieListController with Store {
   final _firestore = Firestore();
-  
-  CollectionReference get _collection => _firestore.collection('lists');
 
   @computed
   ObservableStream<List<MovieListModel>> get movieListsStream => _firestore
@@ -25,15 +24,4 @@ abstract class _MovieListController with Store {
             .toList(),
       )
       .asObservable();
-
-  void add(MovieListModel model) => _collection.add(
-        model.toMap(),
-      );
-
-  void update(String documentId, MovieListModel model) =>
-      _collection.document(documentId).updateData(
-            model.toMap(),
-          );
-
-  void delete(String documentId) => _collection.document(documentId).delete();
 }
